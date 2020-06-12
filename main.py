f8c9cde4e36dccc83f7fbb494552cc9f5c8be57f
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-06-12 15:45:27 +0100

    Made connection a constant

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import psycopg2
 import os
 
-conn = psycopg2.connect(host=os.environ["DB_HOST"],
-                        database=os.environ["DB_NAME"],
-                        user=os.environ["DB_USER"],
-                        password=os.environ["DB_PASSWORD"]
-                        )
+DATABASE_CONNECTION = psycopg2.connect(host=os.environ["DB_HOST"],
+                                       database=os.environ["DB_NAME"],
+                                       user=os.environ["DB_USER"],
+                                       password=os.environ["DB_PASSWORD"]
+                                       )
 
 
 def app(environ, start_response):
-    cur = conn.cursor()
+    cur = DATABASE_CONNECTION.cursor()
     cur.execute('SELECT version()')
     db_version = cur.fetchone()[0]
     cur.close()
