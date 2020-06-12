77695668a24f5b6e122677fe83f519dc3dc10462
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-12 15:19:22 +0100

    Connect to DB

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import psycopg2
+import os
+
+conn = psycopg2.connect(host=os.environ["DB_HOST"],
+                        database=os.environ["DB_NAME"],
+                        user=os.environ["DB_USER"],
+                        password=os.environ["DB_PASSWORD"]
+                        )
+
+
 def app(environ, start_response):
-    data = b"Hello, World!\n"
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()[0]
+    cur.close()
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
+        ("Content-Length", str(len(db_version)))
     ])
-    return iter([data])
+    return iter([str.encode(db_version)])
