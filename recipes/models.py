d5dd4945be67d19d00f051b523e9d2e6aa17cff8
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-14 09:34:13 +0100

    Add initial recipe migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=128)
+    ingredients = models.CharField(max_length=512)
+    method = models.CharField(max_length=8192)
