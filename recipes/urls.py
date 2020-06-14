2a3c7021ed3f4507f4339e40a3617f5e049258d0
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-14 10:18:01 +0100

    Namespace URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:recipe_id>/', views.detail, name='detail'),
