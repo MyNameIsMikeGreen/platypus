a4ac45a6ca691824eaaef22bc1f0abe6b413ec40
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-14 09:20:29 +0100

    Switch to new postgreSQL engine name

--- platypus/settings.py
+++ platypus/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'platypus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ["DB_NAME"],
         'USER': os.environ["DB_USER"],
         'PASSWORD': os.environ["DB_PASSWORD"],
