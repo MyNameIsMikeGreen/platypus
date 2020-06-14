8e8a03d7f433782869bd064f4c88fcd821c2f595
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-14 10:26:37 +0100

    Root maps to recipes

--- platypus/urls.py
+++ platypus/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls')),  # Root redirects to recipes
     path('recipes/', include('recipes.urls'))
 ]
