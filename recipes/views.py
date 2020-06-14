599d89f395ce7c4c76e926c0f7364dec388bef5b
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-06-14 09:54:11 +0100

    Add recipe detail view based on ID

--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,7 @@ def index(request):
     recipe_list = Recipe.objects.order_by('title')
     output = '<br />'.join([recipe.title for recipe in recipe_list])
     return HttpResponse(output)
+
+
+def detail(request, recipe_id):
+    return HttpResponse("You're looking at recipe %s." % recipe_id)
