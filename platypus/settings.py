7f7603d686d1ff189bf8ae89b67374087ebf67c3
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-06-17 20:46:13 +0100

    Use dynamically generated static path. Re-collect static files.

--- platypus/settings.py
+++ platypus/settings.py
@@ -20,6 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# Show exceptions in logs
+# DEBUG_PROPAGATE_EXCEPTIONS = True
+# TASTYPIE_FULL_DEBUG = True
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
