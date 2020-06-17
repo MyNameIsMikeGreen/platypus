b8d3214d5d47b0739bee5bb6038e0425c89d90c7
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-06-17 21:58:38 +0100

    Added support for multiple images to be associated with a recipe and displayed on the details page.

--- recipes/models.py
+++ recipes/models.py
@@ -5,4 +5,9 @@ class Recipe(models.Model):
     title = models.CharField(max_length=128)
     ingredients = models.CharField(max_length=512)
     method = models.CharField(max_length=8192)
-    main_image = models.URLField(max_length=256, null=True)
+
+
+class RecipeImage(models.Model):
+    url = models.URLField(max_length=256, null=True)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    relative_order = models.IntegerField()
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 BULLET_POINT_DELIMITER = ';'
 
@@ -17,10 +17,12 @@ def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     ingredients_list = recipe.ingredients.split(BULLET_POINT_DELIMITER)
     method_list = recipe.method.split(BULLET_POINT_DELIMITER)
+    image_list = RecipeImage.objects.filter(recipe_id=recipe_id).order_by('relative_order')
     context = {
         'recipe': recipe,
         'ingredients_list': ingredients_list,
         'method_list': method_list,
+        'image_list': image_list
     }
     return render(request, 'recipes/detail.html', context)
 
