110554ed6621828cdac0dac8c09fafbfac567276
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-06-23 22:08:15 +0100

    Add custom domain name to allowed hosts

--- platypus/settings.py
+++ platypus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mike-green-platypus.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mike-green-platypus.herokuapp.com', 'mynameismikegreen.co.uk']
 
 # Application definition
 
