7570f8e1826a2b85e152b66fab88bc1dbac9cdf0
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-07-10 17:15:17 +0100

    Container host and port dynamically changed for tests

--- platypustestrunner.py
+++ platypustestrunner.py
@@ -1,14 +1,18 @@
-import sqlalchemy
+import os
+
 from django.test.runner import DiscoverRunner
+import django.conf as conf
+
+
 from testcontainers.postgres import PostgresContainer
 
 
 class PlatypusTestRunner(DiscoverRunner):
 
+    POSTGRESQL_PORT = 5432
+
     def run_tests(self, test_labels, extra_tests=None, **kwargs):
         with PostgresContainer('postgres:12.3') as container:
-            engine = sqlalchemy.create_engine(container.get_connection_url())
-            version, = engine.execute("select version()").fetchone()
-            print(version)
-
+            conf.settings.DATABASES['default']['HOST'] = container.get_container_host_ip()
+            conf.settings.DATABASES['default']['PORT'] = container.get_exposed_port(self.POSTGRESQL_PORT)
             super().run_tests(test_labels, extra_tests=None, **kwargs)
