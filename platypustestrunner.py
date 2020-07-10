caa960739182f6342a374a3edba299677d71aaea
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-07-10 17:42:35 +0100

    Use default username and password for docker postgresql in tests

--- platypustestrunner.py
+++ platypustestrunner.py
@@ -13,4 +13,6 @@ class PlatypusTestRunner(DiscoverRunner):
         with PostgresContainer('postgres:12.3') as container:
             conf.settings.DATABASES['default']['HOST'] = container.get_container_host_ip()
             conf.settings.DATABASES['default']['PORT'] = container.get_exposed_port(self.POSTGRESQL_PORT)
+            conf.settings.DATABASES['default']['USER'] = "test"
+            conf.settings.DATABASES['default']['PASSWORD'] = "test"
             super().run_tests(test_labels, extra_tests=None, **kwargs)
--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -1,12 +1,10 @@
-import unittest
-
 from django.test import TestCase
 from django.test import Client
 
 client = Client()
 
 
-class IndexViewTest(unittest.TestCase):
+class IndexViewTest(TestCase):
     def test_index_exists(self):
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -14,7 +12,7 @@ class IndexViewTest(unittest.TestCase):
         # self.assertInHTML(response, "<p>No recipes are available.</p>")
 
 
-class DetailWithFixturesTest(unittest.TestCase):
+class DetailWithFixturesTest(TestCase):
 
     fixtures = ['recipes.json']
 
@@ -23,14 +21,14 @@ class DetailWithFixturesTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class DetailWithoutFixturesTest(unittest.TestCase):
+class DetailWithoutFixturesTest(TestCase):
 
     def test_details_returns_404_when_recipe_not_present(self):
         response = client.get('/69/')
         self.assertEqual(response.status_code, 404)
 
 
-class AboutViewTest(unittest.TestCase):
+class AboutViewTest(TestCase):
     def test_about_exists(self):
         response = client.get('/about')
         self.assertEqual(response.status_code, 200)
