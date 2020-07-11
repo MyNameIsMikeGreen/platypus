20d90e0e96303d9d9d087d9bbe648617e555b255
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2020-07-11 13:35:53 +0100

    Extra view tests

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -4,11 +4,24 @@ from django.test import Client
 client = Client()
 
 
-class IndexViewTest(TestCase):
+class IndexViewWithFixturesTest(TestCase):
+
+    fixtures = ['recipes.json']
+
     def test_index_exists(self):
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'recipes/index.html')
+
+    def test_index_displays_recipes_list_when_recipes_are_present(self):
+        response = client.get('/')
+        self.assertInHTML('<h1>Recipes</h1>', response.content.decode("utf-8"))
+
+
+class IndexViewWithoutFixturesTest(TestCase):
+    def test_index_displays_error_when_no_recipes_present(self):
+        response = client.get('/')
+        self.assertInHTML('<p>No recipes are available.</p>', response.content.decode("utf-8"))
 
 
 class DetailWithFixturesTest(TestCase):
@@ -18,6 +31,11 @@ class DetailWithFixturesTest(TestCase):
     def test_details_returns_200_when_recipe_present(self):
         response = client.get('/1/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes/detail.html')
+
+    def test_details_without_trailing_slash_redirects(self):
+        response = client.get('/1')
+        self.assertRedirects(response, '/1/', status_code=301)
 
 
 class DetailWithoutFixturesTest(TestCase):
@@ -34,4 +52,4 @@ class AboutViewTest(TestCase):
 
     def test_about_without_trailing_slash_redirects(self):
         response = client.get('/about')
-        self.assertEqual(response.status_code, 301)
+        self.assertRedirects(response, '/about/', status_code=301)
