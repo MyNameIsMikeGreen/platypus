9b119876401dc7bfac0519c997656ba5278049cc
Mike Green <8007279+MyNameIsMikeGreen@users.noreply.github.com>
2020-07-31 22:43:11 +0100

    Categories (#3)
    * Add category field to recipe model
    
    * Rename main meal enum
    
    * Add categories to recipes
    
    * Categorise the index page
    
    * Pluralise category names

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
+UNCATEGORISED = "UNCATEGORISED"
+MAINS = "MAINS"
+DESSERTS = "DESSERTS"
+SNACKS = "SNACKS"
+CATEGORIES = [
+    (UNCATEGORISED, 'Uncategorised'),
+    (MAINS, 'Mains'),
+    (DESSERTS, 'Desserts'),
+    (SNACKS, 'Snacks'),
+]
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=128)
     ingredients = models.CharField(max_length=512)
     method = models.CharField(max_length=8192)
+    category = models.CharField(
+        max_length=32,
+        choices=CATEGORIES,
+        default=UNCATEGORISED,
+    )
 
 
 class RecipeImage(models.Model):
