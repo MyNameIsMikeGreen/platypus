6c73815461fa533ac7000384c87b0d3372405d56
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-11-26 20:14:00 +0000

    Add form to planner page. Drive planner results page using param rather than path.

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -48,35 +48,25 @@ class DetailWithoutFixturesTest(TestCase):
         self.assertEqual(response.status_code, 404, msg="HTTP 404 returned")
 
 
-class PlannerViewTest(TestCase):
-    def test_planner_exists(self):
+class PlannerView(TestCase):
+
+    fixtures = ['recipes.json']
+
+    def test_planner_input_template_used_if_no_params_provided(self):
         response = client.get('/planner/')
         self.assertEqual(response.status_code, 200, msg="HTTP 200 returned")
-        self.assertTemplateUsed(response, 'recipes/planner.html')
+        self.assertTemplateUsed(response, 'recipes/planner_input.html')
+
+    def test_planner_results_template_used_if_valid_recipe_count_param_provided(self):
+        response = client.get('/planner/?recipe_count=3')
+        self.assertEqual(response.status_code, 200, msg="HTTP 200 returned")
+        self.assertTemplateUsed(response, 'recipes/planner_results.html')
 
     def test_planner_without_trailing_slash_redirects(self):
         response = client.get('/planner')
         self.assertRedirects(response, '/planner/', status_code=301)
 
-    def test_planner_has_title(self):
-        response = client.get('/planner/')
-        self.assertInHTML('<h1>Meal Planner</h1>', response.content.decode("utf-8"))
-
-
-class PlannerResultsViewTest(TestCase):
-
-    fixtures = ['recipes.json']
-
-    def test_planner_results_exists(self):
-        response = client.get('/planner/3/')
-        self.assertEqual(response.status_code, 200, msg="HTTP 200 returned")
-        self.assertTemplateUsed(response, 'recipes/planner_results.html')
-
-    def test_planner_results_without_trailing_slash_redirects(self):
-        response = client.get('/planner/3')
-        self.assertRedirects(response, '/planner/3/', status_code=301)
-
-    def test_planner_results_returns_list_number_of_recipes_requested(self):
+    def test_planner_results_returns_list_containing_number_of_recipes_requested(self):
         for recipes_to_request in range(1, 10):
             recipes_list_length = self._count_recipe_links_from_planner(recipes_to_request)
             self.assertEqual(recipes_list_length, recipes_to_request, "Recipe list size equals the recipes requested")
@@ -88,13 +78,13 @@ class PlannerResultsViewTest(TestCase):
 
     @staticmethod
     def _count_recipe_links_from_planner(recipes_to_request):
-        response = client.get(f'/planner/{recipes_to_request}/')
+        response = client.get(f'/planner/?recipe_count={recipes_to_request}')
         response_tree = html.fromstring(response.content.decode("utf-8"))
         return len(response_tree.xpath('//div[@id="platypus-content"]/ul/li'))
 
     @staticmethod
     def _fetch_recipe_links_from_planner(recipe_count):
-        response = client.get(f'/planner/{recipe_count}/')
+        response = client.get(f'/planner/?recipe_count={recipe_count}')
         response_tree = html.fromstring(response.content.decode("utf-8"))
         return response_tree.xpath('//div[@id="platypus-content"]/ul/li/a/@href')
 
--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('planner/', views.planner, name='planner'),
-    path('planner/<int:recipe_count>/', views.planner_results, name='planner_results'),
     path('<int:recipe_id>/', views.detail, name='detail'),
 ]
