1366246b22f71d094e635193170dd7a58db93a78
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-04-09 21:26:03 +0100

    Flush database on deployment

--- deployment/release-actions.sh
+++ deployment/release-actions.sh
@@ -2,5 +2,7 @@
 
 # These actions are intended to be run during the Heroku release phase.
 # The script should not be run manually
+python manage.py makemigrations
+python manage.py flush --no-input
 python manage.py migrate
 python manage.py loaddata recipes.json
