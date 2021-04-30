0129bc8295cd0fbb73b3060610884bd55daedb0c
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-04-30 22:06:12 +0100

    Check if transcrypt installed

--- read-secrets.sh
+++ read-secrets.sh
@@ -1,8 +1,14 @@
-FILE=./.env.enc
-if [ -f $FILE ]; then
-  while read line; do
-    export $line
-  done < $FILE
+ABORT_ERROR_MESSAGE="Skipping attempt to load environment variables from secret. Action may fail if environment variables have not been configured."
+
+if [ -x "$(command -v transcrypt)" ]; then
+  FILE=./.env.enc
+  if [ -f $FILE ]; then
+    while read line; do
+      export $line
+    done < $FILE
+  else
+    echo "$FILE does not exist. $ABORT_ERROR_MESSAGE"
+  fi
 else
-  echo "$FILE does not exist. Action may fail if environment variables have not been configured."
+  echo "Transcrypt is not installed. $ABORT_ERROR_MESSAGE"
 fi
