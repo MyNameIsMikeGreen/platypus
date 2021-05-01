4c11f7c95ce46194fa29a13b08c9ce83a1869425
Mike Green <8007279+MyNameIsMikeGreen@users.noreply.github.com>
2021-05-01 20:22:46 +0100

    Add search bar (#43)
    * POC
    
    * Move search bar to index page. Provide a list of all recipe titles rather than searching database on every keystroke.
    
    * Change search input to form. Handle new 'search_term' param being passed to index page - only list recipes matching search term.
    
    * Permanent redirect for single-result search. Explicit tests.
    
    * Position search bar to right of title

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -4,12 +4,17 @@ from lxml import html
 
 from recipes.models import Recipe
 
+PROD_FIXTURES = 'recipes.json'
+TEST_FIXTURES = 'test_recipes.json'
+
+ENCODING = "utf-8"
+
 client = Client()
 
 
 class IndexViewWithFixturesTest(TestCase):
 
-    fixtures = ['recipes.json']
+    fixtures = [PROD_FIXTURES, TEST_FIXTURES]
 
     def test_index_exists(self):
         response = client.get('/')
@@ -18,25 +23,40 @@ class IndexViewWithFixturesTest(TestCase):
 
     def test_index_displays_recipes_list_when_recipes_are_present(self):
         response = client.get('/')
-        self.assertInHTML('<h1>Recipes</h1>', response.content.decode("utf-8"))
-        self.assertInHTML('<h2>CONFECTIONERY</h2>', response.content.decode("utf-8"))
-        self.assertInHTML('<li><a href="/1/">Ice Cream</a></li>', response.content.decode("utf-8"))
+        response_body = response.content.decode(ENCODING)
+        self.assertInHTML('<h1>Recipes</h1>', response_body)
+        self.assertInHTML('<h2>CONFECTIONERY</h2>', response_body)
+        self.assertInHTML('<li><a href="/1/">Ice Cream</a></li>', response_body)
 
     def test_index_does_not_contain_json_ld_block(self):
         response = client.get('/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertEqual(len(response_tree.xpath('//head/script[@type="application/ld+json"]')), 0, "json-ld block not in HTML")
+
+    def test_index_with_search_term_param_matching_only_one_recipe_redirects_to_detail_page(self):
+        response = client.get('/', {"search_term": "y89234kl"})
+        self.assertRedirects(response, '/recipes/6970/', fetch_redirect_response=False, status_code=301)
+
+    def test_index_with_search_term_param_matching_many_recipes_filters_recipe_lists(self):
+        response = client.get('/', {"search_term": "title"})
+        response_body = response.content.decode(ENCODING)
+        self.assertInHTML('<h1>Recipes</h1>', response_body)
+        self.assertInHTML('<h2>LIGHT DISHES</h2>', response_body)
+        self.assertInHTML('<li><a href="/6969/">Some recipe title</a></li>', response_body)
+        self.assertInHTML('<h2>MAINS</h2>', response_body)
+        self.assertInHTML('<li><a href="/6970/">Unique title 923y89234kljj9</a></li>', response_body)
+        self.assertNotIn('>Ice Cream</a>', response_body)
 
 
 class IndexViewWithoutFixturesTest(TestCase):
     def test_index_displays_error_when_no_recipes_present(self):
         response = client.get('/')
-        self.assertInHTML('<p>No recipes are available.</p>', response.content.decode("utf-8"))
+        self.assertInHTML('<p>No recipes are available.</p>', response.content.decode(ENCODING))
 
 
 class DetailWithFixturesTest(TestCase):
 
-    fixtures = ['recipes.json', 'test_recipes.json']
+    fixtures = [PROD_FIXTURES, TEST_FIXTURES]
 
     def test_details_returns_200_when_recipe_present(self):
         response = client.get('/1/ice-cream/')
@@ -45,7 +65,7 @@ class DetailWithFixturesTest(TestCase):
 
     def test_details_contains_json_ld_block(self):
         response = client.get('/1/ice-cream/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertEqual(len(response_tree.xpath('//head/script[@type="application/ld+json"]')), 1, "json-ld block in HTML")
 
     def test_slug_appended_if_omitted(self):
@@ -58,12 +78,12 @@ class DetailWithFixturesTest(TestCase):
 
     def test_under_development_icon_shown_when_final_flag_false(self):
         response = client.get('/6969/some-recipe-title/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertIn("⚠", response_tree.xpath('//h1')[0].text_content(), "Under development icon is shown")
 
     def test_icon_not_shown_when_final_flag_true(self):
         response = client.get('/1/ice-cream/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertNotIn("&#x26A0;", response_tree.xpath('//h1')[0].text_content(), "Under development icon is not shown")
 
 
@@ -76,7 +96,7 @@ class DetailWithoutFixturesTest(TestCase):
 
 class PlannerView(TestCase):
 
-    fixtures = ['recipes.json']
+    fixtures = [PROD_FIXTURES]
 
     def test_planner_input_template_used_if_no_params_provided(self):
         response = client.get('/planner/')
@@ -85,11 +105,11 @@ class PlannerView(TestCase):
 
     def test_planner_does_not_contain_json_ld_block(self):
         response = client.get('/planner/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertEqual(len(response_tree.xpath('//head/script[@type="application/ld+json"]')), 0, "json-ld block not in HTML")
 
-    def test_planner_does_not_contain_json_ld_block(self):
-        response = client.get('/planner/?recipe_count=3')
+    def test_planner_returns_results_when_recipe_count_provided(self):
+        response = client.get('/planner/', {"recipe_count": 3})
         self.assertEqual(response.status_code, 200, msg="HTTP 200 returned")
         self.assertTemplateUsed(response, 'recipes/planner_results.html')
 
@@ -150,14 +170,14 @@ class PlannerView(TestCase):
 
     @staticmethod
     def _count_recipe_links_from_planner(recipes_to_request):
-        response = client.get(f'/planner/?recipe_count={recipes_to_request}')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response = client.get(f'/planner/', {"recipe_count": recipes_to_request})
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         return len(response_tree.xpath('//div[@id="platypus-content"]/ul/li'))
 
     @staticmethod
     def _fetch_recipe_links_from_planner(recipe_count, category="MAINS"):
-        response = client.get(f'/planner/?recipe_count={recipe_count}&category={category}')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response = client.get(f'/planner/', {"recipe_count": recipe_count, "category": category})
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         return response_tree.xpath('//div[@id="platypus-content"]/ul/li/a/@href')
 
 
@@ -173,7 +193,7 @@ class AboutViewTest(TestCase):
 
     def test_about_does_not_contain_json_ld_block(self):
         response = client.get('/about/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertEqual(len(response_tree.xpath('//head/script[@type="application/ld+json"]')), 0, "json-ld block not in HTML")
 
 
@@ -185,7 +205,7 @@ class Http404Test(TestCase):
 
     def test_about_does_not_contain_json_ld_block(self):
         response = client.get('/thisPageDoesNotExist/')
-        response_tree = html.fromstring(response.content.decode("utf-8"))
+        response_tree = html.fromstring(response.content.decode(ENCODING))
         self.assertEqual(len(response_tree.xpath('//head/script[@type="application/ld+json"]')), 0, "json-ld block not in HTML")
 
 
