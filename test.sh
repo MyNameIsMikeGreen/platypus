e76750822a01f3fdd3526d72c29a19d03a3eb0b2
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-12-25 21:43:08 +0000

    Don't read secrets in test

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-source read-secrets.sh
-
 source venv/bin/activate
-echo "PYTHON VERSION = `python --version`"
+
+export DB_HOST=test.mynameismikegreen.co.uk
+export DB_NAME=travis_ci_test
+export DB_USER=postgres
+export DB_PASSWORD=""
+export DEBUG=True
+export SECRET_KEY=testkey
+
 pip install -r requirements.txt
 python manage.py test --noinput
 deactivate
