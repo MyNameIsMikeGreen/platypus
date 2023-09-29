599e909fb81d143f9d8c19e0c0e5fab9b39b938f
Mike Green <MyNameIsMikeGreen@outlook.com>
2023-09-29 11:44:03 +0100

    Remove python-devel

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source initialiseVirtualEnv.sh
 source read-secrets.sh
 
 source venv/bin/activate
-yum install -y postgresql-libs postgresql postgresql-devel python-devel
+yum install -y postgresql-libs postgresql postgresql-devel
 pip install -r requirements.txt
 gunicorn -w 3 -b 0.0.0.0:8000 platypus.wsgi
 deactivate
