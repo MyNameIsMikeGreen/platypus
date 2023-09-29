53da37ddf5ecc97517906a82ae31f1a1400931ee
Mike Green <MyNameIsMikeGreen@outlook.com>
2023-09-29 11:21:38 +0100

    Add vercel.json

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ source initialiseVirtualEnv.sh
 source read-secrets.sh
 
 source venv/bin/activate
+pip install psycopg2-binary~=2.9.3
 pip install -r requirements.txt
 gunicorn -w 3 -b 0.0.0.0:8000 platypus.wsgi
 deactivate
