59f7a4ea45ef5daf8c416c8d99bcc96f8b50d2f8
Mike Green <MyNameIsMikeGreen@outlook.com>
2023-09-29 11:34:39 +0100

    Download

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ source initialiseVirtualEnv.sh
 source read-secrets.sh
 
 source venv/bin/activate
-pip install psycopg2-binary~=2.9.3
 export PATH="$HOME/.local/bin:$PATH"
 pip install -r requirements.txt
 gunicorn -w 3 -b 0.0.0.0:8000 platypus.wsgi
