b6f3eff445ab1a86ada338d9c5c1075bc29dbc2c
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2024-06-21 14:52:42 +0100

    Basic usage working

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source initialiseVirtualEnv.sh
-source read-secrets.sh
-
-source venv/bin/activate
-pip install -r requirements.txt
-gunicorn -w 3 -b 0.0.0.0:8000 platypus.wsgi
-deactivate
